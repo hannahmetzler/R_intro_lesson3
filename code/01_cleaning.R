87e37c9f52c0af1cdec2db5514a659044dd49558
Hannah Metzler <metzler@csh.ac.at>
2025-02-13 11:08:26 +0100

    dataset with new participants names 2025

--- code/01_cleaning.R
+++ code/01_cleaning.R
@@ -14,11 +14,13 @@ glimpse(data)
 
         
 # ##  This dataset was extracted from the package babynames like this: 
-# library(babynames)
-# data(babynames)
-# data = babynames %>%
-#   filter(name %in% c("Page", "Ashley", "Hannah", "Florian", "Dominco", "Helena", "Rouven", "Stefanie", "Oona", "Karin", "Prabath", "Teresa", "Caroline", "Karina", "Ahmad", "Charmaine", "Mohammad", "Clarice", "Hanne", "Lav", "Magdalena", "Claudia"))
-# readr::write_tsv(data, file = "data/lesson2_data_babynames.txt")
+library(babynames)
+data(babynames)
+data = babynames %>%
+  #select names of this years participants from the big dataframe
+  filter(name %in% c("Ann-Kristin", "Anna", "Dario", "Dominik", "Eirini", "Felipe", "Gina", "Magdalena", 
+                     "Malika", "Pavithren", "Prabath", "Sarafina", "Tamina", "Tanja", "Thomas", "Johannes"))
+readr::write_tsv(data, file = "data/lesson2_data_babynames2025.txt")
 
 ## CLEAN DATA ####
 data_clean = data %>%
